187d89e8c35715843b7a5e60984287935da6a594
RahulRithik <104841781+RahulRithik@users.noreply.github.com>
2022-05-19 11:39:20 +0530

    Update OddnumberUppercase.java

--- week3/day4/OddnumberUppercase.java
+++ week3/day4/OddnumberUppercase.java
@@ -5,8 +5,13 @@ public class OddnumberUppercase {
 	public static void main(String[] args) {
       String text="welcome";
       char [] arr=text.toCharArray();
-      for(int i=0;i<=arr.length-2;i++) {
-    	  
+      for(int i=0;i<arr.length;i++) {
+    	  if(i%2!=0) {
+    		  System.out.println(Character.toUpperCase(arr[i]));
+    	  }
+    	  else {
+    		  System.out.println(Character.toLowerCase(arr[i]));
+    	  }
       }
 	}
 
