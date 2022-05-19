84ba596be7425a628814d61ac1a0ab7ef0ea7feb
RahulRithik <104841781+RahulRithik@users.noreply.github.com>
2022-05-19 22:43:42 +0530

    Add files via upload

---
+++ week2/day4/DuplicateLead.java
@@ -0,0 +1,48 @@
+package week2.day4;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DuplicateLead {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		//login
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		
+		
+		        //crmsfa link
+				driver.findElement(By.linkText("CRM/SFA")).click();
+				String textone=driver.getTitle();
+				System.out.println(textone);
+				
+				//leads and find leads 
+				driver.findElement(By.linkText("Leads")).click();
+				driver.findElement(By.linkText("Find Leads")).click();
+				
+				driver.findElement(By.linkText("Email")).click();
+				driver.findElement(By.name("emailAddress")).sendKeys("rahulrithik666@gmail.com");
+				driver.findElement(By.xpath("//button[contains(text(),'Find Leads')]")).click();
+				Thread.sleep(5000);
+				
+        driver.findElement(By.xpath("(//div[@class='x-grid3-cell-inner x-grid3-col-partyId']/a)[1]")).click();
+                driver.findElement(By.linkText("Duplicate Lead")).click();
+	            String text1=driver.getTitle();			
+                System.out.println(text1);
+                
+                driver.findElement(By.className("smallSubmit")).click();
+                //driver.close();
+	}
+
+}
